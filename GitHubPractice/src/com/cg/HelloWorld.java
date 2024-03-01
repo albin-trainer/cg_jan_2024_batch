7ca0f121cc2db1471900ea2c8cdb631780739ea1
albin-trainer <69448620+albin-trainer@users.noreply.github.com>
2024-03-01 16:14:20 +0530

    Update HelloWorld.java

--- GitHubPractice/src/com/cg/HelloWorld.java
+++ GitHubPractice/src/com/cg/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("Hello");
 	System.out.println("My work on day 2");
-	
+		System.out.println("My work on day 3");
+		System.out.println("My work on day 4");
+
+
 }
 }
